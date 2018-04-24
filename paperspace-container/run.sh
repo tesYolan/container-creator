c837d67059581477533a7f030667bd14eb24b67c
Tesfa Yohannes <tesfa1993@yahoo.com>
2018-04-24 12:31:28 +0300

    Better build file.

--- paperspace-container/run.sh
+++ paperspace-container/run.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-export DISPLAY=:0
-xhost +local:root
+export DISPLAY=:1
+#xhost +local:root
 docker run \
     -it \
     --rm \
     --privileged \
     --runtime nvidia \
-    -p 8000:8000 \
-    -p 8004:6006 \
-    -p 4000:4000 \
-    -p 4242:4242 \
-    -p 11311:11311 \
-    -p 9094:9094 \
-    -p 9090:9090 \
-    --env="DISPLAY=:0" \
+    --env="DISPLAY=:1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    hanson:blender /bin/bash 
-xhost -local:root
+    hanson:screen_layout /bin/bash 
+#xhost -local:root
 #-p 11311:11311 \
+#    -p 8000:8000 \
+#    -p 8004:6006 \
+#    -p 4000:4000 \
+#    -p 4242:4242 \
+#    -p 11311:11311 \
+#    -p 9094:9094 \
+#    -p 9090:9090 \
+#    --volume="/var/run/docker.sock:/var/run/docker.sock" \
--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 xhost +local:root
-nvidia-docker run \
+docker run \
     -it \
     --rm \
     --privileged \
+    --runtime nvidia \
     -p 8000:8000 \
     -p 8004:6006 \
     -p 4000:4000 \
@@ -19,7 +20,8 @@ nvidia-docker run \
     --device=/dev/snd:/dev/snd \
     --device=/dev/video0:/dev/video0 \
     --volume="/home/tesfa/.ssh:/home/hansondev/.ssh" \
+    --volume="/home/tesfa/clone_dir/hr_launchpad:/root/hansonrobotics/hr_launchpad" \
     --user root \
-    hr:simple /bin/bash 
+    hanson:vkws1 /bin/bash 
 xhost -local:root
 #-p 11311:11311 \
